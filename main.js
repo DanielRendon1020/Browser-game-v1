0241968ac2847b6bd5f11b308029a80e978beaa8
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-23 22:10:53 -0700

    new tag in random hole (Finally! 🙃)

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const holes = document.querySelectorAll('.hole')
-// const tags = ['<html>', '<head>', '<body>', '<h1>', '<span>', '<script>', '<link>', '<div>', '<h2>', '<button>', '<title>', '<p>', '<h4>', '<img>', '<a>']
-// const newTag = tags[Math.floor(Math.random() * tags.length)]
-const tag = document.querySelector('.addTag')
+const holes = document.querySelectorAll('.add-tag')
+const tags = ['<html>', '<head>', '<body>', '<h1>', '<span>', '<script>', '<link>', '<div>', '<h2>', '<button>', '<title>', '<p>', '<h4>', '<img>', '<a>']
+const newTag = tags[Math.floor(Math.random() * tags.length)]
+
+console.log(holes)
 
 function randomHole(){
 	holes.forEach(hole => {
-		hole.classList.remove('tag')
+		hole.textContent = ''
 	})
 
 	let newHole = holes[Math.floor(Math.random() * 9)]
-	
-
+	newHole.textContent = newTag
+	console.log(newHole)
 }
 
 randomHole()
