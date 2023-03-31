091c70d13bd5dbdebf76d17078c1ea5a51378c9a
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-30 22:55:28 -0700

    score, timer, tags poping

--- background.js
+++ background.js
@@ -1,73 +1,52 @@
-import {tags, whackedTags} from './main.js'
+import {tags, whackedTags, htmlToText, shuffle} from './main.js'
 
-const allRand = shuffle([...tags, ...whackedTags]) 
+const allRand = shuffle([...tags, ...whackedTags])
 
 
-function shuffle(array) {
-    let counter = array.length;
+const tagText = htmlToText(allRand.join(' '))
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+const mainMarquee = document.querySelector('.main-marquee')
+const marqueeCont = document.querySelector('.marquee-cont')
+const marquee = document.querySelector('.marquee')
 
-        // Decrease counter by 1
-        counter--;
+marqueeCont.innerHTML = tagText
 
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+function makeMore(times, original, appendTo) {
+    for(let i = 0; i < times; i++) {
+        let clone = original.cloneNode(true)
+        appendTo.append(clone);
     }
-
-    return array;
 }
 
-function htmlToText(rawStr) {
-    return rawStr.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-}
+const marqueeClone = makeMore(1, marqueeCont, marquee)
+const moarMarquee = makeMore(16, marquee, mainMarquee)
 
-
-
-const tagText = `<pre>${htmlToText(allRand.join(' '))}</pre>`
-
-const marqueeCont = document.querySelectorAll('.marquee-cont')
-
-marqueeCont.forEach(span => {
-    span.innerHTML = tagText
-})
-
-
-const spans = document.getElementById('spans').cloneNode(true)
-const cloneSpan = document.getElementById('marquee-div').appendChild(spans)
-
-console.log(cloneSpan)
 
 // added event listener because it doesn't get the right width
-addEventListener("load", function () {
- marqueeCont.forEach(el => {
-  // set a default rate, the higher the value, the faster it is
-  let rate = 50;
-  // get the width of the element
-  let distance = el.clientWidth;
-  // get the margin-right of the element
-  let style = window.getComputedStyle(el);
-  let marginRight = parseInt(style.marginRight) || 0;
-  // get the total width of the element
-  let totalDistance = distance + marginRight;
-  // get the duration of the animation 
-  // for a better explanation, see the quoted codepen in the first comment
-  let time = totalDistance / rate;
-  // get the parent of the element
-  let container = el.parentElement;
+// addEventListener("load", function () {
+//  moarMarquee.forEach(el => {
+//   // set a default rate, the higher the value, the faster it is
+//   let rate = 50;
+//   // get the width of the element
+//   let distance = el.clientWidth;
+//   // get the margin-right of the element
+//   let style = window.getComputedStyle(el);
+//   let marginRight = parseInt(style.marginRight) || 0;
+//   // get the total width of the element
+//   let totalDistance = distance + marginRight;
+//   // get the duration of the animation 
+//   // for a better explanation, see the quoted codepen in the first comment
+//   let time = totalDistance / rate;
+//   // get the parent of the element
+//   let container = el.parentElement;
 
-  gsap.to(container, time, {
-   repeat: -1,
-   x: '-'+totalDistance,
-   ease: Linear.easeNone,
-  });
- });
-});
+//   gsap.to(container, time, {
+//    repeat: -1,
+//    x: '-'+totalDistance,
+//    ease: Linear.easeNone,
+//   });
+//  });
+// });
 
 // addEventListener("load", function () {
 //     spans.forEach(el => {
--- main.js
+++ main.js
@@ -1,40 +1,102 @@
 
-const holes = document.querySelectorAll('.add-tag')
+const holes = [...document.querySelectorAll('.add-tag')]
 const tags = ['<html>', '<head>', '<body>', '<h1>', '<span>', '<script>', '<link>', '<div>', '<h2>', '<button>', '<title>', '<p>', '<h4>', '<img>', '<a>', '<iframe>', '<li>', '<canvas>', '<footer>', '<label>', '<nav>']
 const newTag = tags[Math.floor(Math.random() * tags.length)]
 const whackedTags = ['</html>', '</head>', '</body>', '</h1>', '</span>', '</script>', '</link>', '</div>', '</h2>', '</button>', '</title>', '</p>', '</h4>', '</img>', '</a>', '</iframe>', '</li>', '</canvas>', '</footer>', '</label>', '</nav>']
+const scoreCount = document.querySelector('#score span')
+let score = 0
+let timer = document.querySelector('#count-down')
+timer.style.visibility = 'hidden'
+let currentTime = 60
+let countDown = null
+let countDownTimer
 
+function start(){
+	gsap.to("h1", {'text-shadow' : '0px 0px 10px white', yoyo: true, repeat: -1})
+	let startWindow = document.getElementById('start-window')
 
+	let whacking = null
 
+	function timeLeft(){
+		currentTime--
+		timer.textContent = currentTime
+		if(currentTime === 0){
+			clearInterval(countDownTimer)
+		}
+	}
+	
 
+	let startBtn = document.getElementById('start-button')
+	startBtn.addEventListener('click', function() {
+		gsap.to(startWindow, {duration: .8, scale: 0, ease: 'back.in'})
+		gsap.killTweensOf("h1")
+		countDown = setTimeout(() => {
+			timer.style.visibility = 'visible'
+		}, 500)
+		countDownTimer = setInterval(timeLeft, 1000)
+		whacking = setTimeout(() => {
+			whack()
+		}, 1500)
 
-// function start(){
-// 	gsap.to("h1", {'text-shadow' : '0px 0px 8px white', yoyo: true, repeat: -1})
-// 	let startWindow = document.getElementById('start-window')
-// 	let main = document.getElementById('main')
-// 	let startBtn = document.getElementById('start-button')
-// 	startBtn.addEventListener('click', function() {
-// 		gsap.to(startWindow, {duration: .8, scale: 0, ease: 'back.in'})
-// 		gsap.killTweensOf("h1")
-		
-// 	})
-
-// }
-// start()
-
-
-function randomHole(){
-	holes.forEach(hole => {
-		hole.textContent = ''
 	})
 
+}
+start()
+
+function whack(){
+	if(currentTime === 0){
+		return
+	}
+	const hole = Math.floor(Math.random() * holes.length)
+	const newHole = holes[hole]
+	let next = null
+	const newNewTag = tags[Math.floor(Math.random() * tags.length)]
+
+	newHole.innerHTML = htmlToText(newNewTag)
+	gsap.to(newHole, {transform: 'translateY(-170%)', duration:.2, ease: "back.out(2)" })
+	let getTag = tags.indexOf(newNewTag)
+
+	function shake(who) {
+		gsap.fromTo(who, 0.01, {x:-2,}, {x:2, repeat:20})
+	}
+
+	newHole.addEventListener('click', () => {
+		score++
+		scoreCount.textContent = score
+		newHole.innerHTML = htmlToText(whackedTags[getTag])
+		shake(newHole)
+	}, {once:true})
 	
-	let newHole = holes[Math.floor(Math.random() * 9)]
-	newHole.textContent = newTag
-	
-	
+	next = setTimeout(() => {
+		gsap.to(newHole, {transform: 'translateY(20%)', duration: .2})
+		whack()
+	}, 1500)
+
 }
 
-randomHole()
 
-export {tags, whackedTags}
+function htmlToText(rawStr) {
+    return rawStr.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+export {tags, whackedTags, htmlToText, shuffle}
